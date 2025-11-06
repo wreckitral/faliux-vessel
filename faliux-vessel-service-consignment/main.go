92495dc1ec66c448655a6534c5a509bc5ccf83db
wreckitral <defhanayasofhiea@gmail.com>
2025-11-06 20:32:33 +0700

    feat: use go-micro

--- faliux-vessel-service-consignment/main.go
+++ faliux-vessel-service-consignment/main.go
@@ -3,12 +3,9 @@ package main
 import (
 	"context"
 	"log"
-	"net"
-	"sync"
 
 	pb "github.com/wreckitral/faliux-vessel/faliux-vessel-service-consignment/proto/consignment"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
+	"go-micro.dev/v5"
 )
 
 const (
@@ -17,56 +14,65 @@ const (
 
 type repository interface {
 	Create(*pb.Consignment) (*pb.Consignment, error)
+	GetAll() []*pb.Consignment
 }
 
 type Repository struct {
-	mu				sync.RWMutex
 	consignments 	[]*pb.Consignment
 }
 
 func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
-	repo.mu.Lock()
-
 	updated := append(repo.consignments, consignment)
 	repo.consignments = updated
-
-	repo.mu.Unlock()
 
 	return consignment, nil
 }
 
-type service struct {
-	repo repository
-	pb.UnimplementedShippingServiceServer
+func(repo *Repository) GetAll() ([]*pb.Consignment) {
+	return repo.consignments
 }
 
-func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (
-									*pb.Response, error) {
+type consignmentService struct {
+	repo repository
+}
+
+func (s *consignmentService) CreateConsignment(ctx context.Context, req *pb.Consignment,
+							res *pb.Response) error {
 	consignment, err := s.repo.Create(req)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return &pb.Response{Created: true, Consignment: consignment}, nil
+	res.Created = true
+	res.Consignment = consignment
+
+	return nil
+}
+
+func (s *consignmentService) GetConsignments(ctx context.Context, req *pb.GetRequest,
+						res *pb.Response) error {
+	consignments := s.repo.GetAll()
+
+	res.Consignments = consignments
+
+	return nil
 }
 
 func main() {
 	repo := &Repository{}
 
-	lis, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+	service := micro.NewService(
+		micro.Name("faliux.service.consignment"),
+	)
+
+	service.Init()
+
+	if err := pb.RegisterShippingServiceHandler(service.Server(),
+		&consignmentService{repo},); err != nil {
+		log.Panic(err)
 	}
 
-	s := grpc.NewServer()
-
-	// register service with the gRPC server
-	pb.RegisterShippingServiceServer(s, &service{repo: repo})
-
-	reflection.Register(s)
-
-	log.Println("Running on port:", port)
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+	if err := service.Run(); err != nil {
+		log.Panic(err)
 	}
 }
